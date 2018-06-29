2530ab274e7eb21e618319aadf059fbf0b28cef0
Syed Warsi <syed.warsi@mindtree.com>
2018-06-29 23:21:39 +0530

    added autowiring

--- src/main/java/com/jpm/application/controller/BaseController.java
+++ src/main/java/com/jpm/application/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.jpm.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,16 +13,18 @@ import com.jpm.application.service.impl.BaseServiceImpl;
 import com.jpm.application.system.exception.ServiceException;
 
 
+
 @RestController
 @RequestMapping(value = "/v1")
 public class BaseController {
 		
+	@Autowired
+	private BaseServiceImpl baseServiceImpl;
 	
 	@RequestMapping(method = RequestMethod.POST, value="/factorial")
 	@ResponseBody
 	public ResultResponse generateFactorial(@RequestBody UserInput userInput) {
 		try {
-			BaseServiceImpl baseServiceImpl = new BaseServiceImpl();
 			return baseServiceImpl.getFactorial(userInput);
 		} catch (Exception e) {
 			throw new ServiceException(e);
